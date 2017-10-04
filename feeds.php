44404918323f3beb44023f347387c2cab241dbbb
peyo9164 <peyo9164@colorado.edu>
2017-10-03 21:28:38 -0600

    modified:   feeds.php 	new file:   php/autoloader.php 	new file:   php/library/SimplePie.php 	new file:   php/library/SimplePie/Author.php 	new file:   php/library/SimplePie/Cache.php 	new file:   php/library/SimplePie/Cache/Base.php 	new file:   php/library/SimplePie/Cache/DB.php 	new file:   php/library/SimplePie/Cache/File.php 	new file:   php/library/SimplePie/Cache/Memcache.php 	new file:   php/library/SimplePie/Cache/Memcached.php 	new file:   php/library/SimplePie/Cache/MySQL.php 	new file:   php/library/SimplePie/Cache/Redis.php 	new file:   php/library/SimplePie/Caption.php 	new file:   php/library/SimplePie/Category.php 	new file:   php/library/SimplePie/Content/Type/Sniffer.php 	new file:   php/library/SimplePie/Copyright.php 	new file:   php/library/SimplePie/Core.php 	new file:   php/library/SimplePie/Credit.php 	new file:   php/library/SimplePie/Decode/HTML/Entities.php 	new file:   php/library/SimplePie/Enclosure.php 	new file:   php/library/SimplePie/Exception.php 	new file:   php/library/SimplePie/File.php 	new file:   php/library/SimplePie/HTTP/Parser.php 	new file:   php/library/SimplePie/IRI.php 	new file:   php/library/SimplePie/Item.php 	new file:   php/library/SimplePie/Locator.php

--- feeds.php
+++ feeds.php
@@ -2,12 +2,27 @@
 require("include/db.php");
 require("include/header.php");
 require("include/nav.php");
-
+$form = <<< 'HTML'
+<br />
+<br />
+<form action="feeds.php" method="POST">
+    <div align="center">
+        <span id="desc"> Add RSS feed:</span>
+        <input type="text" name="input" placeholder="URL" />
+        <input type="text" name="colnum" placeholder="Column number"/>
+        <input type="submit" value="Submit" />
+    </div>
+</form>
+HTML;
+echo $form;
+$input = $_POST['input'];
+$colnum = $_POST['colnum'];
+$add_query = "INSERT INTO feeds (displayColumn, link) VALUES (" . $colnum . ", '" . $input . "')";
+// echo $add_query;
+Query($db, $add_query);
 // Get all the user's feeds
 $query = "SELECT * FROM feeds";
 $rows = Query($db, $query);
-
-
 function FeedIcon($link)
 {
         // Feed favicon.ico
@@ -15,7 +30,6 @@ function FeedIcon($link)
         if ($url != "") {
                 $imgurl = "https://www.google.com/s2/favicons?domain=";
                 $imgurl .= $url;
-
                 echo "<div class=\"feedsListIcon\">";
                 "\" type=\"image/x-icon\"></div>\n";
                 echo '<img src="';
@@ -23,20 +37,16 @@ function FeedIcon($link)
                 echo '" width="16" height="16" />';
                 echo "</div>\n";
         }
-
-
 }
-
 // Create an array of links and titles
 foreach ($rows as $row) {
 	echo "<article>";
 	$rss = simplexml_load_file($row['link']);
 	if ($rss) {
 		FeedIcon($row['link']);
-
 		if (strlen($rss->channel->title) == 0) {
 			echo "<span class=\"feedsListTitle\">" .
-			    "<a href=\"http://aggregation.co/?feed=" .
+			    "<a href=\"./?feed=" .
 			    $row['id'] .
 			    "\">" .
 			    $row['link'] .
@@ -47,7 +57,7 @@ foreach ($rows as $row) {
 */
 		} else {
 			echo "<span class=\"feedsListTitle\">" .
-			    "<a href=\"http://aggregation.co/?feed=" .
+			    "<a href=\"./?feed=" .
 			    $row['id'] .
 			    "\">" .
 			    $rss->channel->title .
@@ -59,11 +69,9 @@ foreach ($rows as $row) {
 		}
 		echo "<div class=\"feedsListLink\">" .  $row['link'] .
 		    "</div>\n";
-
 	} else {
 		echo "<div>" . $row['link'] . " not found</div>\n";
 	}
 	echo "</article>\n";
 }
-
 require("include/footer.php");
